f296cd60124119674d93868a6faf2870dc2199ce
QasimSandhu <qasimsandhu.itians@gmail.com>
2023-10-06 11:24:17 +0500

    etches student data from multiple APIs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import './App.css';
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import Student from './Student';
+import ReactPaginate from 'react-paginate';
 
-function App(props) {
-  // const [data, setData] = useState(10);
-  const [count, setCount] = useState(100);
-
-  useEffect(() => {
-    // alert("Data is:" + props.data);
-    alert("Count is:" + props.count);
-
-  }, [props.count]);
+function App() {
+    const [users, setUsers] = useState([]);
+    const [totalPages, setTotalPages] = useState(0);
+    const [currentPage, setCurrentPage] = useState(0);
 
 
-  return (
-    <div className="App">
-      <h1>Hooks</h1>
-      <Student count={count} />
-      <button onClick={() => setCount(count + 1)}>Update Count</button>
-      {/* <button onClick={() => setData(data + 1)}>Update Data</button> */}
-    </div>
-  );
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(
+                    `https://reqres.in/api/users?page=${currentPage}`
+                );
+                if (response.status === 200) {
+                    setUsers(response.data.data);
+                    setTotalPages(response.data.total_pages);
+                } else {
+                    console.error('Error fetching data:', response);
+                }
+            } catch (error) {
+                console.error('Error:', error);
+            }
+        };
+
+        fetchData();
+    }, [currentPage]);
+
+    const handlePageChange = (selectedPage) => {
+        setCurrentPage(selectedPage + 1);
+    };
+
+    return (
+        <div className="App">
+            <h1 className='container d-flex justify-content-center'>React component with pagination list</h1>
+            <Student users={users} />
+            <ReactPaginate
+                pageCount={totalPages}
+                pageRangeDisplayed={5}
+                marginPagesDisplayed={2}
+                onPageChange={(selectedItem) => handlePageChange(selectedItem.selected)}
+                containerClassName={'pagination container d-flex justify-content-center align-items-center'}
+                activeClassName={'active'}
+                previousLabel={'Previous'}
+                nextLabel={'Next'}
+                breakLabel={'...'}
+                breakClassName={'break-me'}
+                pageClassName={'page-item'}
+                pageLinkClassName={'page-link'}
+                previousClassName={'page-item'}
+                nextClassName={'page-item'}
+                previousLinkClassName={'page-link'}
+                nextLinkClassName={'page-link'}
+                disabledClassName={'disabled'}
+                className=''
+            />
+        </div>
+    );
 }
 
 export default App;
